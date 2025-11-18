e76175a205eba1eed7923fb983ee86f30ed700e7
Dominic Beck <dmncbck@gmail.com>
2025-11-18 11:39:15 -0800

    Remove duplicate getOptionalParam from adjoint_synthesis_2d

--- matlab/mex/ducc0_sht_adjoint_synthesis_2d_mex.cpp
+++ matlab/mex/ducc0_sht_adjoint_synthesis_2d_mex.cpp
@@ -69,22 +69,6 @@ vector<size_t> build_mstart(size_t lmax, size_t mmax)
     return mstart;
 }
 
-// Helper to get optional parameter
-template<typename T>
-T getOptionalParam(const mxArray *arr, T default_value)
-{
-    if (arr == nullptr || mxIsEmpty(arr)) {
-        return default_value;
-    }
-    if constexpr (is_same_v<T, bool>) {
-        return mxGetScalar(arr) != 0;
-    } else {
-        return static_cast<T>(mxGetScalar(arr));
-    }
-}
-
-// getStringParam is already defined in ducc0_mex_utils.h
-
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
     try {
